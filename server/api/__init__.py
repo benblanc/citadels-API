cd4203c3f16481e4871791808cb29941c71d2ca5
YakubIlyasov <yakub.ilyasov@dropsolid.com>
2021-12-18 18:30:36 +0100

    Imported library

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, request
 from flasgger import Swagger, swag_from
 from flask_restful import Api, Resource, reqparse
+from flask_expects_json import expects_json
 
 from api.utils import helpers
 
