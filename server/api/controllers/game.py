ace82021c1e9deb9f0d34b90baf4a56741614d42
Yakub Ilyasov <yakub.ilyasov@dropsolid.com>
2021-12-18 21:54:28 +0100

    Added more game functions

--- server/api/controllers/game.py
+++ server/api/controllers/game.py
@@ -5,11 +5,36 @@ from api.classes import card
 import api.responses as responses
 
 
-def get_districts(sort_order, limit, offset):
+def get_games(sort_order, limit, offset):
     try:
-        districts = card.ClassCard().get_districts()
+        return
 
-        return apply_query(districts, sort_order, limit, offset)
+    except Exception:
+        logging.error(traceback.format_exc())
+        return responses.internal_server_error()
+
+
+def get_game(game_uuid):
+    try:
+        return
+
+    except Exception:
+        logging.error(traceback.format_exc())
+        return responses.internal_server_error()
+
+
+def create_game(name, description):
+    try:
+        return
+
+    except Exception:
+        logging.error(traceback.format_exc())
+        return responses.internal_server_error()
+
+
+def join_game(game_uuid):
+    try:
+        return
 
     except Exception:
         logging.error(traceback.format_exc())
