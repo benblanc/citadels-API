0b241c836e678e9092003e7a7a8acdda463f1c1b
Yakub Ilyasov <yakub.ilyasov@dropsolid.com>
2021-12-18 21:52:23 +0100

    Added line to actually create the database tables

--- server/main.py
+++ server/main.py
@@ -5,6 +5,8 @@ from api import app, db
 from api.routes import cards, game, status
 from api.models import buildings, cards, characters, deck_characters, deck_discard_pile, deck_districts, game, players, possible_characters, removed_characters, settings
 
+db.create_all()  # create all defined database tables
+
 if __name__ == '__main__':
     try:
         app.run(host='127.0.0.1', port=8080)
