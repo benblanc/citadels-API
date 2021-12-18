d33a6d718f2d4a47ac0582275ecb8e68e2110e38
Yakub Ilyasov <yakub.ilyasov@dropsolid.com>
2021-12-18 21:54:40 +0100

    Fixed typo

--- server/api/models/buildings.py
+++ server/api/models/buildings.py
@@ -6,4 +6,4 @@ class Buildings(db.Model):
     uuid = db.Column(db.String(150), primary_key=True, nullable=False)
     name = db.Column(db.String(100))
     amount = db.Column(db.Integer)
-    player_uuid = db.Column(db.String(150), db.ForeignKey("player.uuid"), nullable=False, index=True)
+    player_uuid = db.Column(db.String(150), db.ForeignKey("players.uuid"), nullable=False, index=True)
--- server/api/models/characters.py
+++ server/api/models/characters.py
@@ -5,4 +5,4 @@ class Characters(db.Model):
     __tablename__ = 'characters'
     uuid = db.Column(db.String(150), primary_key=True, nullable=False)
     name = db.Column(db.String(100))
-    player_uuid = db.Column(db.String(150), db.ForeignKey("player.uuid"), nullable=False, index=True)
+    player_uuid = db.Column(db.String(150), db.ForeignKey("players.uuid"), nullable=False, index=True)
