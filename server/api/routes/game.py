9fe11cbb062c2b6812f4d16268d039b486c24584
Yakub Ilyasov <yakub.ilyasov@dropsolid.com>
2021-12-18 21:54:13 +0100

    Added more game endpoints

--- server/api/routes/game.py
+++ server/api/routes/game.py
@@ -1,6 +1,12 @@
-from api import api, Resource, swag_from, reqparse
+import json
 
-from api.controllers.cards import *
+from api import api, Resource, swag_from, reqparse, request, expects_json
+
+from api.controllers.game import *
+
+from api.models.game import Game as game_db
+
+from api.utils.helpers import *
 
 
 class Games(Resource):
@@ -14,4 +20,31 @@ class Games(Resource):
 
         return get_games(args['sort_order'], args['limit'], args['offset'])
 
+
+class Game(Resource):
+    @swag_from('../templates/index.yml', endpoint='/game/{game_uuid}')
+    def get(self, game_uuid):
+        return get_game(str(game_uuid))
+
+
+class GameCreate(Resource):
+    @swag_from('../templates/index.yml', endpoint='/game/action.create')
+    @expects_json(read_json('api/schemas/game/create.json'))
+    def post(self):
+        body = json.loads(request.data)
+        name = body["name"]
+        description = body["description"]
+
+        return create_game(str(name), str(description))
+
+
+class GameJoin(Resource):
+    @swag_from('../templates/index.yml', endpoint='/game/{game_uuid}/action.join')
+    def post(self, game_uuid):
+        return join_game(str(game_uuid))
+
+
 api.add_resource(Games, '/game')
+api.add_resource(Game, '/game/<string:game_uuid>')
+api.add_resource(GameCreate, '/game/action.create')
+api.add_resource(GameJoin, '/game/<string:game_uuid>/action.join')
