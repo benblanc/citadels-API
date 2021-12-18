7570290b0b379e64a7102e4fe3b70ba10e7530cb
Yakub Ilyasov <yakub.ilyasov@dropsolid.com>
2021-12-18 22:52:03 +0100

    Updated create game function (wip)

--- server/api/controllers/game.py
+++ server/api/controllers/game.py
@@ -1,8 +1,14 @@
 import logging, traceback
 
-from api.classes import card
+from api.classes import game
+
+from api.models.game import Game as game_db
 
 import api.responses as responses
+
+from api.services import database
+
+from api.utils.helpers import *
 
 
 def get_games(sort_order, limit, offset):
@@ -25,7 +31,23 @@ def get_game(game_uuid):
 
 def create_game(name, description):
     try:
-        return
+        game_object = game.ClassGame(create_uuid(), create_timestamp(), name, description)
+
+        success_write_game = database.write_row_to_db(game_db(
+            uuid=game_object.uuid,
+            created=game_object.created,
+            name=game_object.name,
+            description=game_object.description,
+            amount_players=game_object.amount_players,
+            characters_unused=game_object.characters_unused,
+            characters_per_player=game_object.characters_per_player,
+            eight_districts_built=game_object.eight_districts_built,
+            round=game_object.round,
+        ))
+
+        print(success_write_game)
+
+        return responses.db_success_uuid(game_object.uuid)
 
     except Exception:
         logging.error(traceback.format_exc())
