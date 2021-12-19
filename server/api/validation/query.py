d9591e5934e73b1f96d7afe532e5706348ab9d12
Yakub Ilyasov <yakub.ilyasov@dropsolid.com>
2021-12-19 21:30:00 +0100

    Added validation for query parameters

---
+++ server/api/validation/query.py
@@ -0,0 +1,58 @@
+def validate_sort_order(sort_order):
+    valid = False
+    options = ['asc', 'desc']
+
+    if sort_order in options:
+        valid = True
+
+    return valid
+
+
+def validate_order_by(order_by):
+    valid = False
+    options = ['created', 'name']
+
+    if order_by in options:
+        valid = True
+
+    return valid
+
+
+def validate_limit(limit):
+    valid = False
+
+    if limit > 0:
+        valid = True
+
+    return valid
+
+
+def validate_offset(offset):
+    valid = False
+
+    if offset >= 0:
+        valid = True
+
+    return valid
+
+
+def validate_query(sort_order, order_by, limit, offset):
+    invalid_query = None
+
+    if sort_order:  # check if not none
+        if not validate_sort_order(sort_order):
+            invalid_query = 'sort_order'
+
+    if order_by:  # check if not none
+        if not validate_order_by(order_by):
+            invalid_query = 'order_by'
+
+    if limit:  # check if not none
+        if not validate_limit(limit):
+            invalid_query = 'limit'
+
+    if offset:  # check if not none
+        if not validate_offset(offset):
+            invalid_query = 'offset'
+
+    return invalid_query
