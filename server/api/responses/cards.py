18110518f2c42990d40b64959b6b06ab2fbfc161
Yakub Ilyasov <yakub.ilyasov@dropsolid.com>
2021-12-19 22:28:54 +0100

    Improved map readability

--- server/api/responses/cards.py
+++ server/api/responses/cards.py
@@ -1,10 +1,10 @@
 def success_get_cards(districts):
-    response = list(map(lambda x: x.info, districts))
+    response = list(map(lambda district: district.info, districts))
 
     return response, 200
 
 
-def success_get_card(district):
-    response = list(map(lambda x: x.info, district))[0]
+def success_get_card(districts):
+    response = list(map(lambda district: district.info, districts))[0]
 
     return response, 200
