4c3b91e4601878e3c9ebe85a5f61bdce6cae69ee
Yakub Ilyasov <yakub.ilyasov@dropsolid.com>
2021-12-19 16:46:29 +0100

    Moved table creation line

--- server/main.py
+++ server/main.py
@@ -5,10 +5,9 @@ from api import app, db
 from api.routes import cards, game, status
 from api.models import buildings, cards, characters, deck_characters, deck_discard_pile, deck_districts, game, players, possible_characters, removed_characters, settings
 
-db.create_all()  # create all defined database tables
-
 if __name__ == '__main__':
     try:
+        db.create_all()  # create all defined database tables
         app.run(host='127.0.0.1', port=8080)
 
     except Exception:
