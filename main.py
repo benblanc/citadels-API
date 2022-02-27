369b03fdaa0f3bcbfa543c93e14632dfdb649c0b
Yakub Ilyasov <yakub.ilyasov@dropsolid.com>
2022-02-27 21:31:16 +0100

    Removed todo

--- main.py
+++ main.py
@@ -13,5 +13,4 @@ if __name__ == '__main__':
     except Exception:
         logging.error(traceback.format_exc())
 
-# TODO: add order by character order option for removed characters endpoint
 # TODO: at end of game, don't clean up chaaracters etc, so players can observe game state at end of game
