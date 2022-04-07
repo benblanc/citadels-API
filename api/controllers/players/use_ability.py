ec50379f7f96332fa24a293e98bad4e49128fbd8
Yakub Ilyasov <yakub.ilyasov@dropsolid.com>
2022-04-07 12:32:30 +0200

    save progress

--- api/controllers/players/use_ability.py
+++ api/controllers/players/use_ability.py
@@ -53,37 +53,27 @@ def use_ability(game_uuid, player_uuid, main, name_character, name_districts, ot
         if not main and character.ability_additional_income_used:  # check if the player has already used the character's second ability
             return responses.already_used_ability(main=False)
 
-        log = ""
-
-        print(game_uuid, player_uuid, main, name_character, name_districts, other_player_uuid)
-        print("game_uuid: ", game_uuid)
-        print("player_uuid: ", player_uuid)
-        print("main: ", main)
-        print("name_character: ", name_character)
-        print("name_districts: ", name_districts)
-        print("other_player_uuid: ", other_player_uuid)
-
         if main:  # check if character ability was the main ability
             if character.name == ClassCharacterName.assassin.value:  # check if the character is the assassin
-                log = character_helpers.use_assassin_ability(log, game_uuid, name_character, player.name)  # use assassin's ability and update log
+                log = character_helpers.use_assassin_ability(game_uuid, name_character, player.name)  # use assassin's ability and update log
 
                 if isinstance(log, tuple):  # check if error message
                     return log  # in this case it will contain the error message
 
             elif character.name == ClassCharacterName.thief.value:  # check if the character is the thief
-                log = character_helpers.use_thief_ability(log, game_uuid, name_character, player.name)  # use thief's ability and update log
+                log = character_helpers.use_thief_ability(game_uuid, name_character, player.name)  # use thief's ability and update log
 
                 if isinstance(log, tuple):  # check if error message
                     return log  # in this case it will contain the error message
 
             elif character.name == ClassCharacterName.magician.value:  # check if the character is the magician
-                log = character_helpers.use_magician_ability(log, game_uuid, player_uuid, other_player_uuid, player.name, name_districts)  # use magician's ability and update log
+                log = character_helpers.use_magician_ability(game_uuid, player_uuid, other_player_uuid, player.name, name_districts)  # use magician's ability and update log
 
                 if isinstance(log, tuple):  # check if error message
                     return log  # in this case it will contain the error message
 
             elif character.name == ClassCharacterName.warlord.value:  # check if the character is the warlord
-                log = character_helpers.use_warlord_ability(log, game_uuid, player, other_player_uuid, name_districts)  # use warlord's ability and update log
+                log = character_helpers.use_warlord_ability(game_uuid, player, other_player_uuid, name_districts)  # use warlord's ability and update log
 
                 if isinstance(log, tuple):  # check if error message
                     return log  # in this case it will contain the error message
@@ -91,7 +81,7 @@ def use_ability(game_uuid, player_uuid, main, name_character, name_districts, ot
             success_update_character = database.update_row_in_db(characters_db, character.uuid, dict(ability_used=True))  # update ability used flag for character in database
 
         else:  # not main ability
-            log = character_helpers.use_secondary_ability(log, player, character)  # use secondary ability and update log
+            log = character_helpers.use_secondary_ability(player, character)  # use secondary ability and update log
 
             if isinstance(log, tuple):  # check if error message
                 return log  # in this case it will contain the error message
